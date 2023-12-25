7ae11ecf8874530613349128feff609acb5e8733
Victor Eduardo <eduardomedeirosgx@gmail.com>
2023-12-24 22:18:50 -0300

    Criacao de um modal simples

---
+++ src/screens/home/index.tsx
@@ -0,0 +1,12 @@
+import { View } from 'react-native';
+import { styles } from './styles';
+
+import { Simple_Modal } from '../../components/simple_modal';
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Simple_Modal />
+    </View>
+  );
+}
